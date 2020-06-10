7a80e6188bc36b9d5efa4cdfc570f96074d44d6e
Elise Landman (elandman) <elisejlandman@hotmail.com>
2020-06-10 20:08:06 +0200

    Added ip address to log file

--- alert_teams.py
+++ alert_teams.py
@@ -7,4 +7,4 @@ def alert_teams():
     api = WebexTeamsAPI(access_token = teams_token)
 
     message = api.messages.create(toPersonEmail="elisejlandman@hotmail.com",
-               text=None, markdown=("**⚠️ Alert:** " + x + " detected an unknown visitor at " + ti_string + " " + dt_string), files=["alert.png"], attachments=None
+               text=None, markdown=("**⚠️ Alert:** " + x + " detected an unknown visitor at " + ti_string + " " + dt_string + ". " + "Click [here](https://1drv.ms/u/s!Avfnv99SkyA5gbJSm4YXTxfVjn9eYg?e=JlUhXY) to view the log file." ), files=["alert.png"], attachments=None
