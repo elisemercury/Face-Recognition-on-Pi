d19a01ea31e2c6273c0f1f37c56139bcb235cf86
Elise Landman (elandman) <elisejlandman@hotmail.com>
2020-06-10 19:41:35 +0200

    Update alert_teams.py

--- alert_teams.py
+++ alert_teams.py
@@ -3,9 +3,8 @@ from webexteamssdk import WebexTeamsAPI
 def alert_teams():
 
     teams_token = "NTdkMTJlMjUtZjg1ZC00ZWZmLWEwNGYtZjgyNjRmYzk3YzUyMzI5M2Y1ZDQtYzMx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-    #bot_url = "http://127.0.0.1:4040"
 
     api = WebexTeamsAPI(access_token = teams_token)
 
     message = api.messages.create(toPersonEmail="elisejlandman@hotmail.com",
-               text="test", markdown=None, files=["alert.png"], attachments=None)
+               text=None, markdown=("**⚠️ Alert:** " + x + " detected an unknown visitor at " + ti_string + " " + dt_string), files=["alert.png"], attachments=None
