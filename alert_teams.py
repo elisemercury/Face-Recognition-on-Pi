5009f47dd8b45b67493df7df20a1b51bc72b0f15
elisemercury <56441652+elisemercury@users.noreply.github.com>
2020-06-10 18:56:54 +0200

    Update alert_teams.py

--- alert_teams.py
+++ alert_teams.py
@@ -1,15 +1,11 @@
-import webexteamsbot
-import webexteamssdk
 from webexteamssdk import WebexTeamsAPI
 
-def alert_teams()
+def alert_teams():
 
-    bot_email = "alertsbot@webex.bot"
     teams_token = "NTdkMTJlMjUtZjg1ZC00ZWZmLWEwNGYtZjgyNjRmYzk3YzUyMzI5M2Y1ZDQtYzMx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
     #bot_url = "http://127.0.0.1:4040"
-    bot_app_name = "Altert Bot"
 
     api = WebexTeamsAPI(access_token = teams_token)
 
-    message = api.messages.create(toPersonEmail="elandman@cisco.com",
+    message = api.messages.create(toPersonEmail="elisejlandman@hotmail.com",
                text="test", markdown=None, files=["alert.png"], attachments=None)
