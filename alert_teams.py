e01d4f5cdae5473f4b627aed676ae4e65c5b4d42
elisemercury <56441652+elisemercury@users.noreply.github.com>
2020-06-11 23:35:21 +0200

    Update alert_teams.py

--- alert_teams.py
+++ alert_teams.py
@@ -2,9 +2,9 @@ from webexteamssdk import WebexTeamsAPI
 
 def alert_teams(hostname, dt_string, ti_string):
 
-    teams_token = "NTdkMTJlMjUtZjg1ZC00ZWZmLWEwNGYtZjgyNjRmYzk3YzUyMzI5M2Y1ZDQtYzMx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+    teams_token = "TEAMS-TOKEN"
 
     api = WebexTeamsAPI(access_token = teams_token)
 
-    message = api.messages.create(toPersonEmail="elisejlandman@hotmail.com",
+    message = api.messages.create(toPersonEmail="EMAIL",
                text=None, markdown=("**⚠️ Alert:** " + hostname + " detected an unknown visitor at " + ti_string + " " + dt_string + ". "), files=["alert.png"], attachments=None)
